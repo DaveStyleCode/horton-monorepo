70b82dbff77c343f671fe637bf150376de2cf3f2
DaveStyleCode <dave.style.code@gmail.com>
2026-02-05 21:41:05 -0800

    Env Cleanup

--- apps/community-builder/sanity.config.ts
+++ apps/community-builder/sanity.config.ts
@@ -4,15 +4,18 @@ import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./schemas";
 import { structure } from "./desk/structure";
 
-export default defineConfig({
-  name: "community-builder",
-  title: "Community Builder",
-  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID ?? "eeu1q4xq",
-  dataset: import.meta.env.SANITY_STUDIO_DATASET ?? "production",
-  basePath: "/",
-  server: {
-    port: 3333,
+export default defineConfig([
+  {
+    name: "community-builder",
+    title: "Community Builder",
+    projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: import.meta.env.SANITY_STUDIO_DATASET,
+    basePath: "/",
+    server: {
+      port: Number.parseInt(import.meta.env.SANITY_STUDIO_PORT),
+    },
+    plugins: [structureTool({ structure }), visionTool()],
+    schema: { types: schemaTypes },
   },
-  plugins: [structureTool({ structure }), visionTool()],
-  schema: { types: schemaTypes },
-});
+  // Add additional workspaces here to enable the dropdown
+]);
